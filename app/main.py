e64e414caa2d801f6f57e0e525535a9c66841470
Aditya Datta <ADITYADATTAOFFICIAL@GMAIL.COM>
2024-05-22 17:50:30 +0530

    1st stage passed

--- app/main.py
+++ app/main.py
@@ -9,9 +9,7 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-    # print(f"Got connection from {address}")
-    server_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
