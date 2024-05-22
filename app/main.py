6d1ff876b6504ef8964cd37e3f402db2ad3cd19d
Aditya Datta <ADITYADATTAOFFICIAL@GMAIL.COM>
2024-05-22 17:49:18 +0530

    'test'

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
     # print(f"Got connection from {address}")
-    server_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
